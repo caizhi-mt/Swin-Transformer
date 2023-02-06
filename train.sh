404d9c09c106c43e6a20e98c33111f7d77589f6f
Ubuntu <ubuntu@ip-172-31-23-42.us-west-2.compute.internal>
2023-02-06 09:56:12 +0000

    tmp ada

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-horovodrun -np 4 python main_hvd.py --cfg configs/swinv2/swinv2_base_patch4_window8_128_hvd.yaml --data-path ../../datasets/tiny-imagenet-200 --batch-size 64 --disable_amp --local_rank  0
+horovodrun -np 8 python main_hvd.py --cfg configs/swinv2/swinv2_base_patch4_window8_128_hvd.yaml --data-path ../tiny-imagenet-200 --batch-size 32 --disable_amp --local_rank  0
